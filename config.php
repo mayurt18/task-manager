89b91398198068cc66962a33c2f206c11cfaef44
Mayur thakar <140304576+mayurt18@users.noreply.github.com>
2024-11-05 21:03:54 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define('DB_NAME', 'task_manager');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
-// Database connection
+
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 if ($conn->connect_error) {
